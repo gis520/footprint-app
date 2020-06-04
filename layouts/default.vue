ebee308437d2f2fe731a2e4e032e52365328a8c2
giscafer <giscafer@outlook.com>
2020-06-04 22:38:26 +0800

    chore: 布局简单实现

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,81 @@
 <template>
-  <div>
-    <nuxt />
-  </div>
+  <el-container style="height: 100%;">
+    <el-header style="text-align: right; font-size: 12px">
+      <el-dropdown>
+        <i class="el-icon-setting" style="margin-right: 15px"></i>
+        <el-dropdown-menu slot="dropdown">
+          <el-dropdown-item>查看</el-dropdown-item>
+          <el-dropdown-item>新增</el-dropdown-item>
+          <el-dropdown-item>删除</el-dropdown-item>
+        </el-dropdown-menu>
+      </el-dropdown>
+      <span>王小虎</span>
+    </el-header>
+
+    <el-container>
+      <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
+        <el-menu :default-openeds="['1', '3']">
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-message"></i>导航一
+            </template>
+            <el-menu-item-group>
+              <template slot="title">分组一</template>
+              <el-menu-item index="1-1">选项1</el-menu-item>
+              <el-menu-item index="1-2">选项2</el-menu-item>
+            </el-menu-item-group>
+            <el-menu-item-group title="分组2">
+              <el-menu-item index="1-3">选项3</el-menu-item>
+            </el-menu-item-group>
+            <el-submenu index="1-4">
+              <template slot="title">选项4</template>
+              <el-menu-item index="1-4-1">选项4-1</el-menu-item>
+            </el-submenu>
+          </el-submenu>
+          <el-submenu index="2">
+            <template slot="title">
+              <i class="el-icon-menu"></i>导航二
+            </template>
+            <el-menu-item-group>
+              <template slot="title">分组一</template>
+              <el-menu-item index="2-1">选项1</el-menu-item>
+              <el-menu-item index="2-2">选项2</el-menu-item>
+            </el-menu-item-group>
+            <el-menu-item-group title="分组2">
+              <el-menu-item index="2-3">选项3</el-menu-item>
+            </el-menu-item-group>
+            <el-submenu index="2-4">
+              <template slot="title">选项4</template>
+              <el-menu-item index="2-4-1">选项4-1</el-menu-item>
+            </el-submenu>
+          </el-submenu>
+          <el-submenu index="3">
+            <template slot="title">
+              <i class="el-icon-setting"></i>导航三
+            </template>
+            <el-menu-item-group>
+              <template slot="title">分组一</template>
+              <el-menu-item index="3-1">选项1</el-menu-item>
+              <el-menu-item index="3-2">选项2</el-menu-item>
+            </el-menu-item-group>
+            <el-menu-item-group title="分组2">
+              <el-menu-item index="3-3">选项3</el-menu-item>
+            </el-menu-item-group>
+            <el-submenu index="3-4">
+              <template slot="title">选项4</template>
+              <el-menu-item index="3-4-1">选项4-1</el-menu-item>
+            </el-submenu>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+
+      <el-main>
+        <h3>布局演示</h3>
+        <!-- nuxt 相当于路由，nuxt.js 会编译别的页面插入此处 -->
+        <nuxt />
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <style>
@@ -23,7 +97,18 @@ html {
   box-sizing: border-box;
   margin: 0;
 }
-
+html,
+body,
+#__nuxt,
+#__layout,
+#__layout {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+.custom-page {
+  height: 100%;
+}
 .button--green {
   display: inline-block;
   border-radius: 4px;
@@ -51,5 +136,14 @@ html {
 .button--grey:hover {
   color: #fff;
   background-color: #35495e;
+}
+.el-header {
+  background: #1b2034;
+  color: #fff;
+  line-height: 60px;
+}
+
+.el-aside {
+  color: #333;
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,27 +2,11 @@
   <div class="container">
     <div>
       <logo />
-      <h1 class="title">
-        footprint-app
-      </h1>
-      <h2 class="subtitle">
-        旅游足迹网站
-      </h2>
+      <h1 class="title">footprint-app</h1>
+      <h2 class="subtitle">旅游足迹网站</h2>
       <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
+        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
       </div>
     </div>
   </div>
@@ -41,7 +25,7 @@ export default {
 <style>
 .container {
   margin: 0 auto;
-  min-height: 100vh;
+  max-height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
